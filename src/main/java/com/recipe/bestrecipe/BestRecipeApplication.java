838f0aed70ae73efa07126c0a33ae624c2185a2a
Dragos <dragosinfinit10@gmail.com>
2026-05-17 01:53:09 +0300

    initial commit

---
+++ src/main/java/com/recipe/bestrecipe/BestRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.bestrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recipe/bestrecipe/BestRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.bestrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
