f923b23116eaeaf3d3d27b33623c279b24ed3b78
Dragos <dragosinfinit10@gmail.com>
2026-05-17 19:20:43 +0300

    Add getAllRecipes()

---
+++ src/main/java/com/recipe/bestrecipe/services/RecipeService.java
@@ -0,0 +1,9 @@
+package com.recipe.bestrecipe.services;
+
+import com.recipe.bestrecipe.models.Recipe;
+
+import java.util.List;
+
+public interface RecipeService {
+    List<Recipe> getAllRecipes();
+}
